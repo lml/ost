88416d64b62cfd147a308e4c8b50003a78b6c7bd
JP Slavinsky <jpslav@gmail.com>
2012-07-28 17:14:14 -0700

    got rid of unneeded files

--- app/helpers/educators_helper.rb
+++
@@ -1,7 +0,0 @@
-module EducatorsHelper
-  def type(educator)
-    return "instructor" if educator.is_instructor
-    return "teaching_assistant" if educator.is_teaching_assistant
-    return "grader" if educator.is_grader
-  end
-end
--- test/unit/offered_course_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class OfferedCourseTest < ActiveSupport::TestCase
-
-  test 'basic' do
-    o = Factory
-  end
-end
