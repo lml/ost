c06c16ef4e4ba9f724996b3285246105e1c33d12
JP Slavinsky <jpslav@gmail.com>
2012-08-14 00:31:53 -0700

    initializers

---
+++ config/initializers/amazon_ses.rb
@@ -0,0 +1,3 @@
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+  :access_key_id     => SECRET_SETTINGS[:aws_ses_access_key_id],
+  :secret_access_key => SECRET_SETTINGS[:aws_ses_secret_access_key]
