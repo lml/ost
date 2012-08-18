1891ad0b7094fd6808cf8cc0a2e6e5943425c581
JP Slavinsky <jpslav@gmail.com>
2012-08-18 14:12:18 -0700

    contact us page

--- app/controllers/write_controller.rb
+++ app/controllers/write_controller.rb
@@ -1,5 +1,7 @@
 class WriteController < ApplicationController
+  skip_before_filter :authenticate_user!, :only => [:index]
+  
   def index
-    redirect_to "mailto:#{params[:to]}@openstaxcollege.org"
+    redirect_to "mailto:#{params[:to]}@openstaxtutor.org"
   end
 end
