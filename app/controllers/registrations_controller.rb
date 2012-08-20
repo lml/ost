c9ecbceb45c974b47d728b9e592730c611d71090
JP Slavinsky <jpslav@gmail.com>
2012-08-20 09:57:44 -0700

    sign up button fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,24 +15,29 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource
 
-    resource.username = params[:user][:username]
-
-    @enable_recaptcha = @@enable_recaptcha
-
-    if (!@enable_recaptcha || verify_recaptcha(:model => resource,
-         :message => "Your answer did not match the reCAPTCHA. Please try again.")) &&
-       resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up
-        sign_in_and_redirect(resource_name, resource)
-      else
-        set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s
-        expire_session_data_after_sign_in!
-        redirect_to after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords(resource)
+    if !(params[:agreement_checkbox] == "1")
+      flash[:alert] = "You must agree to the terms to sign up."
       render 'new'
+    else
+      resource.username = params[:user][:username]
+
+      @enable_recaptcha = @@enable_recaptcha
+
+      if (!@enable_recaptcha || verify_recaptcha(:model => resource,
+           :message => "Your answer did not match the reCAPTCHA. Please try again.")) &&
+         resource.save
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up
+          sign_in_and_redirect(resource_name, resource)
+        else
+          set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s
+          expire_session_data_after_sign_in!
+          redirect_to after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords(resource)
+        render 'new'
+      end
     end
   end
 
