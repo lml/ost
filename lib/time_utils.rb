52879259761d25039efd3fd0b8720da848467ee1
Kevin Burleigh <klb@kindling>
2012-08-24 15:36:56 -0700

    AssignmentPlan update/create form updates; AssignmentPlan/Klass start/end time checking updates; adjusted scenario full_learning_plan accordingly

--- lib/time_utils.rb
+++ lib/time_utils.rb
@@ -1,33 +1,49 @@
 module TimeUtils
-  def self.time_and_zone_strings_to_utc_time(origin_time_string, origin_zone_string)
-    original_chronic_time_class = Chronic.time_class
-    origin_zone = ActiveSupport::TimeZone.new(format_zone_string(origin_zone_string))
-    raise "unable to parse origin zone string: (#{origin_zone_string})" if origin_zone.nil?
-    origin_time = Chronic.parse(origin_time_string)
-    raise "unable to parse origin time string: (#{origin_time_string})" if origin_time.nil?
-    origin_zone.local_to_utc(origin_time)
-  ensure
-    Chronic.time_class = original_chronic_time_class
+
+  def self.timestr_and_zonestr_to_utc_time(origin_timestr, origin_zonestr)
+    origin_time = Chronic.parse(origin_timestr)
+    raise "unable to parse origin time string: (#{origin_timestr})" if origin_time.nil?
+    self.time_and_zonestr_to_utc_time(origin_time, origin_zonestr)
   end
 
-  def self.time_to_time_in_zone_string(time, target_zone_string, time_format_string='%Y-%m-%d %H:%M:%S')
-    target_zone = ActiveSupport::TimeZone.new(target_zone_string)
-    raise "unable to parse target zone string: #{target_zone_string}" if target_zone.nil?
-    target_zone.utc_to_local(time.utc).strftime(time_format_string)
+  def self.time_and_zonestr_to_utc_time(origin_time, origin_zonestr)
+    origin_zone = TimeUtils.zonestr_to_zone(origin_zonestr)
+    self.time_and_zone_to_utc_time(origin_time, origin_zone)    
   end
   
-  def self.time_to_time_string(time, time_format_string='%Y-%m-%d %H:%M:%S')
-    time.strftime(time_format_string)
+  def self.time_and_zone_to_utc_time(time, zone)
+    zone.local_to_utc(time)
   end
   
-  def self.zone_to_string(zone)
-    format_zone_string(zone.to_s)
+  def self.time_and_zonestr_to_timestr_in_zone(time, target_zonestr, time_formatstr='%Y-%m-%d %H:%M')
+    target_zone = ActiveSupport::TimeZone.new(target_zonestr)
+    raise "unable to parse target zone string: #{target_zonestr}" if target_zone.nil?
+    target_zone.utc_to_local(time.utc).strftime(time_formatstr)
   end
   
-  def self.format_zone_string(str)
-    if /^\(.*?\)\s+(?<rest>.*)$/ =~ str
-      str = rest
+  def self.time_to_timestr(time, time_formatstr='%Y-%m-%d %H:%M')
+    timestr = time.strftime(time_formatstr)
+    raise "invalid time format string (#{time_formatstr})" \
+      if timestr.blank?
+    timestr
+  end
+
+  def self.zonestr_to_zone(zonestr)
+    zone = ActiveSupport::TimeZone.new(format_zonestr(zonestr));
+    raise "unable to parse origin zone string: (#{zonestr})" \
+      if zone.nil?
+    zone
+  end
+  
+  def self.zone_to_zonestr(zone)
+    self.format_zonestr(zone.to_s)
+  end
+  
+  def self.format_zonestr(zonestr)
+    if /^\(.*?\)\s+(?<rest>.*)$/ =~ zonestr
+      zonestr = rest
     end
-    str
+    zonestr
   end
+  
 end
