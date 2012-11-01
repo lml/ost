b59f11892de17dfc2007818d8e2a74e8ab49442c
JP Slavinsky <jpslav@gmail.com>
2012-11-01 11:50:39 -0700

    accordion view in instructor getting started

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -8,4 +8,26 @@ module HelpHelper
     @sections[options[:section]].push({:question => options[:question], :answer => capture(&block)})
   end
 
+  def accordion_entry(options, &block)
+    @accordion_entry_count ||= 0
+
+    raise IllegalArgument if options[:title].nil?
+    content_tag :div, {:class => 'accordion-group'} do
+      @content = content_tag(:div, :class => 'accordion-heading') do
+        content_tag :a, :class => 'accordion-toggle', 
+                    :'data-toggle' => 'collapse', 
+                    :href => "#accordion_entry_#{@accordion_entry_count+=1}" do
+          options[:title]
+        end
+      end
+      @content << content_tag(:div, :id => "accordion_entry_#{@accordion_entry_count}",
+                                    :class => 'accordion-body collapse') do
+        content_tag :div, :class => 'accordion-inner' do
+          capture(&block)
+        end
+      end
+    end
+ 
+  end
+
 end
