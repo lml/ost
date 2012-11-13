2a3952171dd4edf1d3c366ef4c3a05bc00faa1c4
Kevin Burleigh <klb@kindling>
2012-11-13 05:34:09 -0800

    expanded test coverage

--- spec/models/percent_scheduler_spec.rb
+++ spec/models/percent_scheduler_spec.rb
@@ -43,7 +43,7 @@ describe PercentScheduler do
                     scheduler.schedules.size.should eq 0
                     schedule = scheduler.add_schedule
                     scheduler.schedules.size.should eq 1
-                    scheduler.schedules.last.should == schedule
+                    scheduler.schedules.last.should equal(schedule)
                 end
             end
             context "scheduler has schedules" do
@@ -53,7 +53,7 @@ describe PercentScheduler do
                     scheduler.schedules.size.should eq 2
                     schedule = scheduler.add_schedule
                     scheduler.schedules.size.should eq 3
-                    scheduler.schedules.last.should == schedule
+                    scheduler.schedules.last.should equal(schedule)
                 end
             end
         end
@@ -110,7 +110,7 @@ describe PercentScheduler do
 
                         scheduler.schedules.size.should eq 3
                         scheduler.schedules[schedule_index].size.should eq 2
-                        scheduler.schedules[schedule_index].last.should == schedule_row
+                        scheduler.schedules[schedule_index].last.should equal(schedule_row)
                         scheduler.schedules[schedule_index].last.should eq({:percent => 0, :tags => ""})
                         row_number.should eq 2
                     end
@@ -138,8 +138,203 @@ describe PercentScheduler do
     end # context
 
     context "assignment distribution" do
-        describe "#build_assignment" do
-            
+        describe "#build_assignment(assignment_plan, cohort)" do
+            context "AssignmentPlan is a test" do
+                it "raises IllegalOperation" do
+                    assignment_plan = nil
+                    cohort          = nil
+                    scheduler       = nil
+
+                    DbUniverse do
+                        DbCofClass start_date: "Sep 1, 2012 5:00am", end_date: "Dec 1, 2012 5:00pm" do
+                            assignment_plan = DbCofAssignmentPlan is_test: true
+                            cohort          = DbCofCohort() do
+                                DbCofLearningCondition do
+                                    scheduler = DbCofPercentScheduler()
+                                end 
+                            end
+                        end
+                    end
+
+                    expect {
+                        scheduler.build_assignment(assignment_plan, cohort)
+                    }.to raise_error(IllegalOperation)
+                end
+            end
+            context "there are no schedules" do
+                it "raises IllegalArgument" do
+                    assignment_plan = nil
+                    cohort          = nil
+                    scheduler       = nil
+
+                    DbUniverse do
+                        DbCofClass start_date: "Sep 1, 2012 5:00am", end_date: "Dec 1, 2012 5:00pm" do
+                            assignment_plan = DbCofAssignmentPlan is_test: false
+                            cohort          = DbCofCohort() do
+                                DbCofLearningCondition do
+                                    scheduler = DbCofPercentScheduler()
+                                end 
+                            end
+                        end
+                    end
+
+                    expect {
+                        scheduler.build_assignment(assignment_plan, cohort)
+                    }.to raise_error(IllegalArgument)
+                end
+            end
+            context "there are no TopicExercises" do
+                it "returns a well-formed empty Assignment" do
+                    assignment_plan = nil
+                    cohort          = nil
+                    scheduler       = nil
+
+                    DbUniverse do
+                        DbCofClass start_date: "Sep 1, 2012 5:00am", end_date: "Dec 1, 2012 5:00pm" do
+                            assignment_plan = DbCofAssignmentPlan is_test: false
+                            cohort          = DbCofCohort() do
+                                DbCofLearningCondition do
+                                    scheduler = DbCofPercentScheduler() do |scheduler|
+                                        scheduler.schedules = [[ {:percent => 100, :tags => "tag1"} ]]
+                                        scheduler.save!
+                                    end
+                                end 
+                            end
+                        end
+                    end
+
+                    assignment = scheduler.build_assignment(assignment_plan, cohort)
+
+                    assignment.assignment_plan.should equal(assignment_plan)
+                    assignment.cohort.should equal(cohort)
+                    assignment.assignment_exercises.size.should eq 0                
+                end
+            end
+            it "chooses the appropriate schedule" do
+                assignment_plan1 = nil
+                assignment_plan2 = nil
+                assignment_plan3 = nil
+                cohort           = nil
+                scheduler        = nil
+
+                DbUniverse do
+
+                    exercise = DbCofExercise url: "http://quadbase.org/questions/q4668v1"
+
+                    DbCofClass start_date: "Sep 1, 2012 5:00am", end_date: "Dec 1, 2012 5:00pm" do
+                        DbCofLearningPlan do
+                            topic1 = DbCofTopic name: "Topic One" do
+                                DbCofTopicExercise name: "Topic One Exercise One",   exercise: exercise
+                            end
+                            topic2 = DbCofTopic name: "Topic Two" do
+                                DbCofTopicExercise name: "Topic Two Exercise One",   exercise: exercise
+                            end
+                            topic3 = DbCofTopic name: "Topic Three" do
+                                DbCofTopicExercise name: "Topic Three Exercise One", exercise: exercise
+                            end
+
+                            assignment_plan1 = DbCofAssignmentPlan   starts_at:  "Sep 1, 2012 6:00am", 
+                                                                     ends_at:    "Sep 3, 2012 10:00pm",
+                                                                     is_test:    false do
+                                DbCofAssignmentPlanTopic topic: topic1
+                            end
+                            assignment_plan2 = DbCofAssignmentPlan   starts_at:  "Sep 2, 2012 6:00am", 
+                                                                     ends_at:    "Sep 4, 2012 10:00pm",
+                                                                     is_test:    false do
+                                DbCofAssignmentPlanTopic topic: topic2
+                            end
+                            assignment_plan3 = DbCofAssignmentPlan   starts_at:  "Sep 3, 2012 6:00am", 
+                                                                     ends_at:    "Sep 5, 2012 10:00pm",
+                                                                     is_test:    false do
+                                DbCofAssignmentPlanTopic topic: topic3
+                            end
+                            cohort          = DbCofCohort() do
+                                DbCofLearningCondition do
+                                    scheduler = DbCofPercentScheduler() do |scheduler|
+                                        scheduler.schedules = [[ {:percent => 100, :tags => "tag1"} ],
+                                                               [ {:percent => 100, :tags => "tag2"} ],
+                                                               [ {:percent => 100, :tags => "tag3"} ]]
+                                        scheduler.save!
+                                    end
+                                end 
+                            end
+                        end
+                    end
+                end
+
+                assignment1 = scheduler.build_assignment(assignment_plan1, cohort)
+                assignment2 = scheduler.build_assignment(assignment_plan2, cohort)
+                assignment3 = scheduler.build_assignment(assignment_plan3, cohort)
+
+                assignment1.assignment_exercises.each {|ae| ae.tag_list.find{|tag| tag == "tag1"}.should be_true}
+                assignment2.assignment_exercises.each {|ae| ae.tag_list.find{|tag| tag == "tag2"}.should be_true}
+                assignment3.assignment_exercises.each {|ae| ae.tag_list.find{|tag| tag == "tag3"}.should be_true}
+            end
+            context "AssignmentPlan covers multiple Topics" do
+                it "pulls TopicExercises from each Topic until scheduled percentage is reached (but not exceeded)" do
+                    assignment_plan = nil
+                    cohort          = nil
+                    scheduler       = nil
+
+                    DbUniverse do
+
+                        exercise = DbCofExercise url: "http://quadbase.org/questions/q4668v1"
+
+                        DbCofClass start_date: "Sep 1, 2012 5:00am", end_date: "Dec 1, 2012 5:00pm" do
+                            DbCofLearningPlan do
+                                topic1 = DbCofTopic name: "Topic One" do
+                                    DbCofTopicExercise name: "Topic One Exercise One",   exercise: exercise
+                                    DbCofTopicExercise name: "Topic One Exercise Two",   exercise: exercise
+                                    DbCofTopicExercise name: "Topic One Exercise Three", exercise: exercise
+                                end
+                                topic2 = DbCofTopic name: "Topic Two" do
+                                    DbCofTopicExercise name: "Topic Two Exercise One",   exercise: exercise
+                                    DbCofTopicExercise name: "Topic Two Exercise Two",   exercise: exercise
+                                    DbCofTopicExercise name: "Topic Two Exercise Three", exercise: exercise
+                                    DbCofTopicExercise name: "Topic Two Exercise Four",  exercise: exercise
+                                end
+                                topic3 = DbCofTopic name: "Topic Three" do
+                                    DbCofTopicExercise name: "Topic Three Exercise One", exercise: exercise
+                                end
+
+                                assignment_plan = DbCofAssignmentPlan   starts_at:  "Sep 1, 2012 6:00am", 
+                                                                        ends_at:    "Sep 3, 2012 10:00pm",
+                                                                        is_test:    false do
+                                    DbCofAssignmentPlanTopic topic: topic1
+                                    DbCofAssignmentPlanTopic topic: topic2
+                                    DbCofAssignmentPlanTopic topic: topic3
+                                end
+                                cohort          = DbCofCohort() do
+                                    DbCofLearningCondition do
+                                        scheduler = DbCofPercentScheduler() do |scheduler|
+                                            scheduler.schedules = [[ {:percent => 50, :tags => "tag1"} ]]
+                                            scheduler.save!
+                                        end
+                                    end 
+                                end
+                            end
+                        end
+                    end
+
+                    assignment = scheduler.build_assignment(assignment_plan, cohort)
+
+                    assignment.assignment_plan.should equal(assignment_plan)
+                    assignment.cohort.should equal(cohort)
+                    assignment.assignment_exercises.size.should eq 3
+
+                    assignment.assignment_exercises.any?{|ae| ae.topic_exercise.name == "Topic One Exercise One"  }.should be_true
+                    assignment.assignment_exercises.any?{|ae| ae.topic_exercise.name == "Topic One Exercise Two"  }.should be_false
+                    assignment.assignment_exercises.any?{|ae| ae.topic_exercise.name == "Topic One Exercise Three"}.should be_false
+
+                    assignment.assignment_exercises.any?{|ae| ae.topic_exercise.name == "Topic Two Exercise One"  }.should be_true
+                    assignment.assignment_exercises.any?{|ae| ae.topic_exercise.name == "Topic Two Exercise Two"  }.should be_true
+                    assignment.assignment_exercises.any?{|ae| ae.topic_exercise.name == "Topic Two Exercise Three"}.should be_false
+                    assignment.assignment_exercises.any?{|ae| ae.topic_exercise.name == "Topic Two Exercise Four" }.should be_false
+
+                    assignment.assignment_exercises.any?{|ae| ae.topic_exercise.name == "Topic Three Exercise One"}.should be_false
+                end
+            end
         end
+
     end # context
 end
