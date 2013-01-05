27363e76de93ecdc64e723ff4b2ed5c1e333ccb3
Kevin Burleigh <klb@kindling>
2013-01-04 23:09:22 -0600

    AssignmentExercise unique index on TopicExercise scoped by Assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130105043104) do
+ActiveRecord::Schema.define(:version => 20130105050407) do
 
   create_table "assignment_coworkers", :force => true do |t|
     t.integer  "student_assignment_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(:version => 20130105043104) do
   end
 
   add_index "assignment_exercises", ["assignment_id"], :name => "index_assignment_exercises_on_assignment_id"
-  add_index "assignment_exercises", ["topic_exercise_id"], :name => "index_assignment_exercises_on_topic_exercise_id"
+  add_index "assignment_exercises", ["topic_exercise_id", "assignment_id"], :name => "index_assignment_exercises_on_topic_exercise_id_scoped", :unique => true
 
   create_table "assignment_plan_topics", :force => true do |t|
     t.integer  "assignment_plan_id",                      :null => false
