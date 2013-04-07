8cca0c836b09f193f662924df2b2beb48d008257
Kevin Burleigh <klb@kindling>
2013-04-06 20:17:50 -0700

    added begin/end log messages to cron tasks

--- lib/cron_jobs.rb
+++ lib/cron_jobs.rb
@@ -18,18 +18,42 @@ module Ost
     end
 
     def execute_5min_cron_jobs
+      Rails.logger.info "CRON: #{Time.now} begin 5 min cron jobs"
+
+      Rails.logger.info "CRON: #{Time.now} begin AssignmentPlanbuild_and_distribute_assignments"
       AssignmentPlan.build_and_distribute_assignments
+      Rails.logger.info "CRON: #{Time.now} end   AssignmentPlanbuild_and_distribute_assignments"
+
+      Rails.logger.info "CRON: #{Time.now} begin create_missing_student_assignments"
       Assignment.create_missing_student_assignments
+      Rails.logger.info "CRON: #{Time.now} end   create_missing_student_assignments"
+
+      Rails.logger.info "CRON: #{Time.now} end   5 min cron jobs"
     end
 
     def execute_30min_cron_jobs
+      Rails.logger.info "CRON: #{Time.now} begin 30 min cron jobs"
+
+      Rails.logger.info "CRON: #{Time.now} begin StudentAssignment.note_if_due!"
       StudentAssignment.note_if_due!
+      Rails.logger.info "CRON: #{Time.now} end   StudentAssignment.note_if_due!"
+
+      Rails.logger.info "CRON: #{Time.now} begin MailHook.destroy_all_expired!"
       MailHook.destroy_all_expired!
+      Rails.logger.info "CRON: #{Time.now} end   MainlHook.destroy_all_expired!"
+
+      Rails.logger.info "CRON: #{Time.now} end   30 min cron jobs"
     end
 
     def execute_60min_cron_jobs
+      Rails.logger.info "CRON: #{Time.now} begin 60 min cron jobs"
+
+      Rails.logger.info "CRON: #{Time.now} begin ScheduledNotificationMailer.send!"
       ScheduledNotificationMailer.send!
+      Rails.logger.info "CRON: #{Time.now} end   ScheduledNotificationMailer.send!"
+
+      Rails.logger.info "CRON: #{Time.now} end   60 min cron jobs"
     end
 
-  end  
+  end
 end
