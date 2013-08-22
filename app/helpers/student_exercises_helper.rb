c15fc1b1a988a2998671598077d88079763c2279
Kevin Burleigh <klb@kindling>
2013-08-22 11:39:34 -0700

    updates StudentExercise and StudentExercise Feedback navigation

--- app/helpers/student_exercises_helper.rb
+++ app/helpers/student_exercises_helper.rb
@@ -6,17 +6,42 @@ module StudentExercisesHelper
     ["Definitely-Wrong", "Probably-Wrong", "Maybe Wrong,-Maybe Right", "Probably-Right", "Definitely-Right"]
   end
 
+  def student_exercise_index(student_exercise)
+    student_exercise.assignment_exercise.number - 1
+  end
+
   def prev_student_exercise(student_exercise)
-    index = student_exercise.assignment_exercise.number - 1
+    index = student_exercise_index(student_exercise)
     if index > 0
       student_exercise.student_assignment.student_exercises[index-1]
     end
   end
 
   def next_student_exercise(student_exercise)
-    index = student_exercise.assignment_exercise.number - 1
+    index = student_exercise_index(student_exercise)
     if index < student_exercise.student_assignment.student_exercises.count - 1
       student_exercise.student_assignment.student_exercises[index+1]
     end
+  end
+
+  def available_student_exercise_feedback_path(student_exercise)
+    return nil if student_exercise.nil?
+    student_exercise.is_feedback_available? ? student_exercise_feedback_path(student_exercise) : nil
+  end
+
+  def prev_available_student_exercise_feedback_path(student_exercise)
+    index = student_exercise_index(student_exercise)
+    if index > 0
+      ses = student_exercise.student_assignment.student_exercises[0..index-1]
+      se = ses.reverse.detect{|se| se.is_feedback_available?}
+      available_student_exercise_feedback_path(se)
+    end
+  end
+
+  def next_available_student_exercise_feedback_path(student_exercise)
+    index = student_exercise_index(student_exercise)
+    ses = Array(student_exercise.student_assignment.student_exercises[index+1..-1])
+    se = ses.detect{|se| se.is_feedback_available?}
+    available_student_exercise_feedback_path(se)
   end
 end
