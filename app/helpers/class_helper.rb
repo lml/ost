24eebf8eae732109315847c71b91142666348dc2
Kevin Burleigh <klb@kindling>
2013-10-01 17:33:31 -0700

    hides non-consenting Student ids from researchers in report.xls

--- app/helpers/class_helper.rb
+++ app/helpers/class_helper.rb
@@ -2,4 +2,15 @@
 # License version 3 or later.  See the COPYRIGHT file for details.
 
 module ClassHelper
+  def report_coworkers(student_assignment, user=present_user)
+    students = student_assignment.assignment_coworkers.collect{|acw| acw.student}.concat([student_assignment.student])
+    student_names = students.collect do |student|
+      if user.is_researcher?
+        student.consented? ? student.full_name(user) : "non-consenting student"
+      else
+        student.full_name(user)
+      end
+    end
+    student_names.sort_by{|name| name.downcase}.join(',')
+  end
 end
