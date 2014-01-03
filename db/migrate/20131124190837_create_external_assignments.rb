fd0b4b37c6011041b287837fe6e515c616fb1805
JP Slavinsky <jpslav@gmail.com>
2014-01-02 16:34:13 -0800

    fixed empty reg status bug on EAE, added DB indices, added topics to EAs

--- db/migrate/20131124190837_create_external_assignments.rb
+++ db/migrate/20131124190837_create_external_assignments.rb
@@ -7,13 +7,20 @@ class CreateExternalAssignments < ActiveRecord::Migration
       t.timestamps
     end
 
+    add_index :external_assignments, :klass_id
+
     create_table :external_assignment_exercises do |t|
       t.integer     :external_assignment_id,  null: false
       t.integer     :concept_id
+      t.integer     :topic_id
       t.integer     :number
       t.string      :name,                    limit: 100
       t.timestamps
     end
+
+    add_index :external_assignment_exercises, :external_assignment_id, name: 'index_eae_on_external_assignment_id'
+    add_index :external_assignment_exercises, :concept_id, name: 'index_eae_on_concept_id'
+    add_index :external_assignment_exercises, :topic_id, name: 'index_eae_on_topic_id'
 
     create_table :student_external_assignments do |t|
       t.integer     :external_assignment_id,  null:     false
@@ -22,11 +29,17 @@ class CreateExternalAssignments < ActiveRecord::Migration
       t.timestamps
     end
 
+    add_index :student_external_assignments, :external_assignment_id, name: 'index_sea_on_external_assignment_id'
+    add_index :student_external_assignments, :student_id, name: 'index_sea_on_student_id'
+
     create_table  :student_external_assignment_exercises do |t|
       t.integer     :external_assignment_exercise_id, null:     false
       t.integer     :student_external_assignment_id,  null:     false
       t.float       :grade,                           default:  0.0
       t.timestamps
     end
+
+    add_index :student_external_assignment_exercises, :external_assignment_exercise_id, name: 'index_seae_on_eae_id'
+    add_index :student_external_assignment_exercises, :student_external_assignment_id, name: 'index_seae_on_sea_id'
   end
 end
