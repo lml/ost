d0f432431491acb82e7651229a8d1978b16cb6a0
lakshmivyas <lakshmi.vyas@gmail.com>
2014-05-19 19:17:21 +0530

    lml/ost#341: Add aggregated assignment exercise view.
    - Add permissions to assignment exercise model.
    - Add links in the assignment page for the new view.
    - Add assignment exercise controller with a single show action.
    - Add assignment exercise view.
    
    Notes:
    
    - This commit introduces a lot of redundancy between student
      exercise and the assignment exercise.  Need to refactor to
      partials for better reuse.
    
    - Assignment exercises are bunched for a particular cohort,
      need to make it accessible for all cohorts.
    
    - The table border rendering is flaky in FF nightly.

---
+++ app/controllers/assignment_exercises_controller.rb
@@ -0,0 +1,12 @@
+# Copyright 2011-2014 Rice University. Licensed under the Affero General Public
+# License version 3 or later.  See the COPYRIGHT file for details.
+
+
+class AssignmentExercisesController < ApplicationController
+
+  def show
+    @assignment_exercise = AssignmentExercise.find(params[:id])
+    raise SecurityTransgression unless present_user.can_read?(@assignment_exercise)
+  end
+
+end
--- app/models/assignment_exercise.rb
+++ app/models/assignment_exercise.rb
@@ -1,23 +1,23 @@
-# Copyright 2011-2012 Rice University. Licensed under the Affero General Public 
+# Copyright 2011-2012 Rice University. Licensed under the Affero General Public
 # License version 3 or later.  See the COPYRIGHT file for details.
 
 class AssignmentExercise < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :topic_exercise
   has_many :student_exercises, :dependent => :destroy
-  
+
   acts_as_taggable
-  
+
   # only checking for the assignment_id on update so that we can build up an
   # entire assignment and save it all together (assignment gets saved last, so
   # the assignment ID doesn't exist in time to be approved by this validation)
   validates :assignment_id, :presence => true, :on => :update
   validates :topic_exercise_id, :presence => true, :uniqueness => {:scope => :assignment_id}
-  
+
   before_destroy :destroyable?, prepend: true
-  
+
   acts_as_numberable :container => :assignment
-  
+
   attr_accessible :assignment, :topic_exercise, :assignment_id
 
   def destroyable?
@@ -26,13 +26,21 @@ class AssignmentExercise < ActiveRecord::Base
       if student_exercises(true).any?
     errors.none?
   end
-  
+
   # Expects comma-delimted tags, eg. "tag1, tag2"
   def add_tags(tags)
     tag_list.add(tags, :parse => true) if !tags.blank?
   end
-  
+
   def has_tag?(tag)
     tag_list.include?(tag.downcase)
   end
+
+  #############################################################################
+  # Access control methods
+  #############################################################################
+  def can_be_read_by?(user)
+    !user.is_anonymous? && (assignment.is_educator?(user) || user.is_administrator?)
+  end
+
 end
