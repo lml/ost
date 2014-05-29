ed123dba53a28649adbdc28674811a52146e4e5b
JP Slavinsky <jpslav@gmail.com>
2014-05-29 14:01:14 -0700

    changed dev server to port 3010

--- config/boot.rb
+++ config/boot.rb
@@ -7,3 +7,15 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+module Rails
+  class Server
+
+    DEV_PORT = 3010
+
+    alias :default_options_alias :default_options
+    def default_options
+      default_options_alias.merge!(:Port => DEV_PORT)
+    end    
+  end
+end
