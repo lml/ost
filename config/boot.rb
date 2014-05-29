2c65099a4c8f553f5dc57d796cdd586164efc76f
JP Slavinsky <jpslav@gmail.com>
2014-05-29 14:35:15 -0700

    forgot a require

--- config/boot.rb
+++ config/boot.rb
@@ -8,6 +8,8 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
+require 'rails/commands/server'
+
 module Rails
   class Server
 
