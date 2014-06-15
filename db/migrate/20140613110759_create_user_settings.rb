ad65a41de61db4427d75b06751c8f39aacfd58aa
lakshmivyas <lakshmi.vyas@gmail.com>
2014-06-15 15:18:51 +0530

    lml/ost#347: Fix the continue button action.
    - Add a hidden field to make sure the `save_and_lock` parameter
      expected by the controller is provided.
    - Rename `AddUserSettings` => `CreateUserSettings`
    - Remove `null => true` according to @dantemss' comment.

--- db/migrate/20140613110759_create_user_settings.rb
+++ db/migrate/20140613110759_create_user_settings.rb
@@ -1,11 +1,11 @@
 # Copyright 2011-2014 Rice University. Licensed under the Affero General Public
 # License version 3 or later.  See the COPYRIGHT file for details.
 
-class AddUserSettings < ActiveRecord::Migration
+class CreateUserSettings < ActiveRecord::Migration
   def change
     create_table(:user_settings) do |t|
       t.integer :user_id, :null => false
-      t.text    :settings, :null => true
+      t.text    :settings
       t.timestamps
     end
 
